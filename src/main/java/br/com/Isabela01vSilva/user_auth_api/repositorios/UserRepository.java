48352d0c5c4e7d4fb99237abff378859f9f5c235
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-07-04 17:45:36 -0300

    Config User

---
+++ src/main/java/br/com/Isabela01vSilva/user_auth_api/repositorios/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.Isabela01vSilva.user_auth_api.repositorios;
+
+import br.com.Isabela01vSilva.user_auth_api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin();
+}
